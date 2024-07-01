3ca52f7b0135eed375b05d5fed84d589f0620af5
exquo <62397152+exquo@users.noreply.github.com>
2024-07-01 10:56:22 +0000

    Improve yazi wrapper
    And put the common functions and info in common.sh

--- contrib/yazi-wrapper.sh
+++ contrib/yazi-wrapper.sh
@@ -1,47 +1,30 @@
-!#/usr/bin/env bash
-
-set -x
+#!/bin/sh
 
 # This wrapper script is invoked by xdg-desktop-portal-termfilechooser.
-#
-# Inputs:
-# 1. "1" if multiple files can be chosen, "0" otherwise.
-# 2. "1" if a directory should be chosen, "0" otherwise.
-# 3. "0" if opening files was requested, "1" if writing to a file was
-#    requested. For example, when uploading files in Firefox, this will be "0".
-#    When saving a web page in Firefox, this will be "1".
-# 4. If writing to a file, this is recommended path provided by the caller. For
-#    example, when saving a web page in Firefox, this will be the recommended
-#    path Firefox provided, such as "~/Downloads/webpage_title.html".
-#    Note that if the path already exists, we keep appending "_" to it until we
-#    get a path that does not exist.
-# 5. The output path, to which results should be written.
-#
-# Output:
-# The script should print the selected paths to the output path (argument #5),
-# one path per line.
-# If nothing is printed, then the operation is assumed to have been canceled.
 
-multiple="$1"
-directory="$2"
-save="$3"
-path="$4"
-out="$5"
-cmd="yazi"
-termcmd="${TERMCMD:-wezterm start --always-new-process}"
+# See `common.sh` for the description of the input arguments and the output format.
+
+set -eu
+
+. "${0%/*}/common.sh"  # source common functions
+
+multiple=$1
+directory=$2
+save=$3
+path=$4
+out=$5
+
+cmd='yazi'
+termcmd=$(default_termcmd)
 
 if [ "$save" = "1" ]; then
-    ARGS=--chooser-file="$out"
+	create_save_file "$path" "$out"
+	set -- --chooser-file="$out" "$path"
 elif [ "$directory" = "1" ]; then
-    ARGS=--cwd-file="$out"
-elif [ "$multiple" = "1" ]; then
-    ARGS=--chooser-file="$out"
+	set -- --chooser-file="$out" --cwd-file="$out"
+	# Returns either the dir explicitly selected (with e.g. <Enter>), or the last dir yazi was in before exiting.
 else
-    ARGS=--chooser-file="$out"
+	set -- --chooser-file="$out"
 fi
 
-$termcmd $cmd $ARGS
-
-if [ "$save" = "1" ] && [ ! -s "$out" ]; then
-    /usr/bin/rm "$path"
-fi
+$termcmd $cmd "$@"
